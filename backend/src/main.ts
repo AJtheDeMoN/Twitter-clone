92a440204688cff7c1dca44e80f205faf25a7446
AJtheDeMoN <ajayathwal911@gmail.com>
2024-07-04 23:34:56 +0530

    first

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -9,7 +9,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: 'http://localhost:3000', // Allow requests from this origin
+    origin: true, // Allow requests from any origin
     methods: 'GET,POST,PUT,DELETE,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization',
   });
