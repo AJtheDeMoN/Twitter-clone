9371dd960c5f3f63af62912ed4d3ebf1756dbdfc
Ajay Athwal <97742675+AJtheDeMoN@users.noreply.github.com>
2024-07-04 23:16:22 +0530

    Update env.config.ts

--- backend/src/configs/env.config.ts
+++ backend/src/configs/env.config.ts
@@ -2,7 +2,7 @@ import { registerAs } from '@nestjs/config';
 
 export default registerAs('CONFIGS', () => ({
   DATABASE_URL:
-    'mongodb+srv://demonrage725:iNAo4f759eDgSsvx@cluster0.tisppw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0',
+    'mongodb+srv://demonrage725:iNAo4f759eDgSsvx@cluster0.tisppw9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0?directConnection=true',
   PORT: 3001,
   JWT_SECRET: '2EhG8u5VqW54W4Ux9A3f7RbE6S2e0BdK',
 }));
